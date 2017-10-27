57a92b79669b8296c34e2bdf202c2c0e115bf10e
taranO <33162073+taranO@users.noreply.github.com>
2017-10-28 00:04:07 +0200

    Add files via upload

---
+++ AMP.m
@@ -0,0 +1,77 @@
+function [ x_est, E ] = AMP(x0, x, A, y, lambda, T, stepsize, gamma, is_zero, is_all_iterations)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% x_est = argmin ||Ax-y||^2 + lambda ||x||^2 
+%
+% PARAMS:
+%
+% - x0 : side information;  
+% - x  : original signal;
+% - A  : transform matrix;
+% - y  : qunatised-sparsified representation of x;
+% - lambda : Lagrangian multiplier for side information;
+% - T  : number of iterations;
+% - stepsize : start step size of descent;
+% - gamma    : decresing speed of step size;  
+% - is_zero  : type of start initialisation: 0 - start from prior; 1 - strart from zero
+% - is_all_iterations :  1 - finish after T iteration; 0 - finish after convergence;
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% --- Functions -----------------------------------------------------------
+F = @(z, A, y) 0.5*(norm(A*z - y)^2+lambda*norm(z-x0)^2);
+% -------------------------------------------------------------------------
+
+E     = [];  % estimated error 
+iter  = 0;   % number of iterations
+
+n     = size(A, 2);
+B     = A'*A + lambda*eye(n);
+C     = A'*y + lambda*x0;
+
+v     = zeros(size(y));
+x_cur = zeros(size(x0));
+if is_zero
+    x_prev = zeros(size(x0));
+else
+    x_prev  = x0;    
+end
+
+while (is_all_iterations && iter < T) || ...
+        (is_all_iterations  == 0 && (iter == 0 || (iter < T && norm(x-x_prev) > norm(x-x_cur)))) 
+    if iter ~= 0
+        x_prev = x_cur;
+    end
+    iter = iter + 1;
+    
+    b = norm(x_prev)/n;
+    v = y - A*x_prev + b*v;    
+        
+    gradvalue = B*x_prev - C; 
+    numTrials = 1;
+    newStep   = stepsize;    
+    
+    x_cur       = x_prev - newStep*gradvalue  + A'*b*v;
+    Fprev       = F(x_prev,A,y);
+    difference  = F(x_cur,A,y) - Fprev ...
+                - gradvalue'*(x_cur-x_prev) - (1/(2*newStep))*norm(x_cur-x_prev)^2; 
+    % backtracking line search 
+    while numTrials < 500 && difference > 0,
+        numTrials   = numTrials + 1;
+        newStep     = newStep*gamma;
+        x_cur       = x_prev - newStep*gradvalue  + A'*b*v;
+        difference  = F(x_cur,A,y) - Fprev ...
+            - gradvalue'*(x_cur-x_prev) - (1/(2*newStep))*norm(x_cur-x_prev)^2;
+    end
+
+    E(iter) = norm(x-x_cur);
+end
+
+if is_all_iterations == 0 && length(E) > 1
+    E(end) = [];
+end
+
+x_est = x_prev;
+
+end
+
